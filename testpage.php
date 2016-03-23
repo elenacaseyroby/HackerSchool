69444faa372d66f189d034430fa415cd77e64037
elenacaseyroby <elenacaseyroby@gmail.com>
2016-03-23 00:46:30 -0700

    recognizes sentences now unless period is immediately followed by quotes (or any character I guess)

--- testpage.php
+++ testpage.php
@@ -24,7 +24,7 @@ function search_text($keyword, $textblock){
 
 	foreach($textblockarray as $word){
 
-		if(strtolower( $word) == strtolower($keyword)){
+		if(strtolower($word) == strtolower($keyword)){
 			$savesentence = 1;
 		}
 
@@ -35,8 +35,6 @@ function search_text($keyword, $textblock){
 
 		if($lastcharacter == "."){
 			$word = substr_replace($word, "", -1);
-
-			echo "<br> last word of sentence: ". $word;
 
 			if(strtolower($word) == strtolower($keyword)){
 				$savesentence = 1;
@@ -81,33 +79,7 @@ Search text for keyword: <input type="text" name="keyword" value=<?= isset($_GET
 </form>
 
 <?php 
-$textblock = "For a long time, the story goes, we supported a Victorian
-regime, and we continue to be dominated by it even today. Thus the image of the imperial prude is emblazoned on our
-restrained, mute, and hypocritical sexuality. At the beginning of the seventeenth century a certain
-frankness was still common, it would seem. Sexual practices
-had little need of secrecy; words were said without undue
-reticence, and things were done without too much concealment;
-one had a tolerant familiarity with the illicit. Codes
-regulating the coarse, the obscene, and the indecent were
-quite lax compared to those ofthe nineteenth century. It was
-a time of direct gestures, shameless discourse, and open
-transgressions, when anatomies were shown and intermingled
-at will, and knowing children hung about amid the
-laughter of adults: it was a period when bodies 'made a
-display of themselves.' But twilight soon fell upon this bright day, followed by the
-monotonous nights of the Victorian bourgeoisie. Sexuality
-was carefully confined; it moved into the home. The conjugal
-family took custody of it and absorbed it into the serious
-function of reproduction. On the subject of sex, silence became
-the rule. The legitimate and procreative couple laid
-down the law. The couple imposed itself as model, enforced
-the norm, safeguarded the truth, and reserved the right to
-speak while retaining the principle of secrecy. A single locus
-of sexuality was acknowledged in social space as well as at
-the heart of every household, but it was a utilitarian and
-fertile one: the parents' bedroom. The rest had only to remain
-vague; proper demeanor avoided contact with other
-bodies, and verbal decency sanitized one's speech."; 
+$textblock = "For a long time, the story goes, we supported a Victorian regime, and we continue to be dominated by it even today. Thus the image of the imperial prude is emblazoned on our restrained, mute, and hypocritical sexuality. At the beginning of the seventeenth century a certain frankness was still common, it would seem. Sexual practices had little need of secrecy; words were said without undue reticence, and things were done without too much concealment; one had a tolerant familiarity with the illicit. Codes regulating the coarse, the obscene, and the indecent were quite lax compared to those ofthe nineteenth century. It was a time of direct gestures, shameless discourse, and open transgressions, when anatomies were shown and intermingled at will, and knowing children hung about amid the laughter of adults: it was a period when bodies 'made a display of themselves.' But twilight soon fell upon this bright day, followed by the monotonous nights of the Victorian bourgeoisie. Sexuality was carefully confined; it moved into the home. The conjugal family took custody of it and absorbed it into the serious function of reproduction. On the subject of sex, silence became the rule. The legitimate and procreative couple laid down the law. The couple imposed itself as model, enforced the norm, safeguarded the truth, and reserved the right to speak while retaining the principle of secrecy. A single locus of sexuality was acknowledged in social space as well as at the heart of every household, but it was a utilitarian and fertile one: the parents' bedroom. The rest had only to remain vague; proper demeanor avoided contact with other bodies, and verbal decency sanitized one's speech."; 
 
 $keyword = $_GET['keyword'];
 
